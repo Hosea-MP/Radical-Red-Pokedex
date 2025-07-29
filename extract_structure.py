3fc1713c64e10db1d4b725186e0bd579a1f8cc49
hoseamp <76258414+Hosea-MP@users.noreply.github.com>
2025-07-29 08:36:28 -0500

    Add script to extract structure of data.js

---
+++ extract_structure.py
@@ -0,0 +1,35 @@
+import ast
+import sys
+
+def traverse(obj, prefix=""):
+    if isinstance(obj, dict):
+        keys = list(obj.keys())
+        # If all keys look numeric, treat as list-like and only use first key
+        if all(isinstance(k, (int, float)) or str(k).isdigit() for k in keys):
+            if keys:
+                first = keys[0]
+                new_prefix = f"{prefix}.[item]" if prefix else "[item]"
+                print(new_prefix)
+                traverse(obj[first], new_prefix)
+            return
+        for key, val in obj.items():
+            new_prefix = f"{prefix}.{key}" if prefix else str(key)
+            print(new_prefix)
+            traverse(val, new_prefix)
+    elif isinstance(obj, list) and obj:
+        new_prefix = prefix + "[]"
+        print(new_prefix)
+        traverse(obj[0], new_prefix)
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python extract_structure.py <file>")
+        return
+    with open(sys.argv[1], 'r', encoding='utf-8') as f:
+        data = f.read()
+    obj = ast.literal_eval(data)
+    traverse(obj)
+
+if __name__ == '__main__':
+    main()
