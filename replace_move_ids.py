75d6c18767062c968498d5c2f15685740a045596
hoseamp <76258414+Hosea-MP@users.noreply.github.com>
2025-07-29 09:39:57 -0500

    Map abilities and egg groups to names

--- replace_move_ids.py
+++ replace_move_ids.py
@@ -10,6 +10,17 @@ with open(INPUT, 'r', encoding='utf-8') as f:
 # Map of move ID -> move name
 move_map = {int(mid): m.get('name', mid) for mid, m in data.get('moves', {}).items()}
 
+# Maps for abilities and egg groups
+ability_map = {}
+for aid, info in data.get('abilities', {}).items():
+    if isinstance(info, dict):
+        name = info.get('name') or (info.get('names') or [aid])[0]
+    else:
+        name = info
+    ability_map[int(aid)] = name
+
+egg_map = {int(eid): name for eid, name in data.get('eggGroups', {}).items()}
+
 # Lookup tables for tutor and TM move indices to actual move IDs
 tutor_lookup = data.get('tutorMoves', {})
 tm_lookup = data.get('tmMoves', {})
@@ -28,6 +39,20 @@ for mon in data.get('species', {}).values():
 
     if 'levelupMoves' in mon:
         mon['levelupMoves'] = [[move_map.get(mid, mid), lvl] for mid, lvl in mon['levelupMoves']]
+
+    if 'abilities' in mon:
+        mon['abilities'] = [[ability_map.get(aid, aid), slot] for aid, slot in mon['abilities']]
+
+    if 'eggGroup' in mon:
+        mon['eggGroup'] = [egg_map.get(eid, eid) for eid in mon['eggGroup']]
+
+# Update trainer Pokemon abilities
+for trainer in data.get('trainers', {}).values():
+    for mode in ('normal', 'hardcore'):
+        if mode in trainer:
+            for poke in trainer[mode]:
+                if 'ability' in poke:
+                    poke['ability'] = ability_map.get(poke['ability'], poke['ability'])
 
 with open(OUTPUT, 'w', encoding='utf-8') as f:
     f.write(repr(data))
